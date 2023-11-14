176e62dc84c79b5d87886e9b6ed236656ff37ea8
GODrums <armin.stanitzok@outlook.de>
2023-11-14 05:45:14 +0100

    Add missing tier in overprints

--- src/modules/OverprintMapping.ts
+++ src/modules/OverprintMapping.ts
@@ -3,7 +3,7 @@ import { StandardMapping, AbstractPattern } from './StandardMapping';
 interface OverprintPattern extends AbstractPattern {
     img: string;
     type: 'Polygon' | 'Arrow' | 'Mixed' | 'Flower';
-    tier: 1 | 2 | 3;
+    tier: 0 | 1 | 2 | 3;
 }
 
 class OverprintModule extends StandardMapping<OverprintPattern> {
